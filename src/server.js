e8cb9693078f73c2c7080ac26070820f433616db
Lucas Siqueira Surreço <112952518+LucasSiqueiraSurreco@users.noreply.github.com>
2023-09-17 16:23:53 -0300

    First Commit

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import http from "node:http";
+
+const port = 3333;
+const users = [];
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/users") {
+    return res
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
+  }
+
+  if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: "johndoe@example.com",
+    });
+    return res.writeHead(201).end();
+  }
+});
+
+server.listen(port);
